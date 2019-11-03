0b517f30bcb82296568e88df4c688902aa6892df
AnaGMendesPedroso <anagmendesp@gmail.com>
2019-11-03 18:29:35 -0300

    Servidor .. a saga começou

--- src/redes/servidor/GerenciadorDiretorios.java
+++ src/redes/servidor/GerenciadorDiretorios.java
@@ -4,21 +4,22 @@ import java.io.File;
 
 public class GerenciadorDiretorios {
 
-    public static void criarDiretorio(String caminho) {
+    public GerenciadorDiretorios(){}
+    public  String criarDiretorio(String caminho) {
         File  sistemaDeArquivos = new File(caminho);
         boolean deu = sistemaDeArquivos.mkdir();
-        if (deu) System.out.println("Diretorio criado! ");
-        else System.out.println("Deu ruim..");
+        if (deu) return "Diretorio criado no caminho "+caminho;
+        else return "Deu ruim ao tentar criar o diretório "+caminho;
     }
 
-    public static void removerDiretorio(String caminho) {
+    public  void removerDiretorio(String caminho) {
         File  sistemaDeArquivos = new File(caminho);
         boolean deu =  sistemaDeArquivos.delete();
         if (deu) System.out.println("Diretorio removido");
         else System.out.println("Deu ruim  na remoção...");
     }
 
-    public static void listarConteudoDiretorio(String caminho) {
+    public  void listarConteudoDiretorio(String caminho) {
         File sistemaArquivos = new File(caminho);
         String [] resultado = sistemaArquivos.list();
         for (String s: resultado ) {
--- src/redes/servidor/UDPServer.java
+++ src/redes/servidor/UDPServer.java
@@ -1,15 +1,12 @@
 package redes.servidor;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
 public class UDPServer {
+   private  static  GerenciadorDiretorios gerenciadorDiretorios = new GerenciadorDiretorios();
     public static void main(String args[]) {
         DatagramSocket aSocket = null;
         try {
@@ -17,13 +14,16 @@ public class UDPServer {
             byte[] buffer = new byte[5000];
             while (true) {
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                System.out.println("PASSOU AQUI: "+new String(request.getData()));
+                String[] acaoConteudo = new String(request.getData()).split("#");
+                System.out.println("Servidou recebeu o pacotão com a ação: " + acaoConteudo[0]);
+                System.out.println("Servidou recebeu o pacotinho com o conteúdo : " + acaoConteudo[1]);
                 aSocket.receive(request);
-                DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(),
-                        request.getPort());
-                
-                salvarArquivo(request.getData(), request.getLength());
-                aSocket.send(reply);
+
+                if (acaoConteudo[0].equalsIgnoreCase("1")) {
+                    String result = gerenciadorDiretorios.criarDiretorio( acaoConteudo[1]);
+                    DatagramPacket reply = new DatagramPacket(result.getBytes(), result.length(), request.getAddress(), request.getPort());
+                    aSocket.send(reply);
+                }
             }
         } catch (SocketException e) {
             System.out.println("Socket: " + e.getMessage());
@@ -32,26 +32,6 @@ public class UDPServer {
         } finally {
             if (aSocket != null)
                 aSocket.close();
-        }
-    }
-
-    public static void salvarArquivo(byte[] bytes, int tamanho) throws IOException {
-        byte[] bytesCorte = new byte[tamanho];
-        for(int i=0; i<tamanho; i++)
-            bytesCorte[i] = bytes[i];
-
-        File f = new File ("/home/nathan/Documentos/DevPC/Redes/Servidor/nomeDoArquivo.java");
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream (f);
-            fos.write (bytesCorte);
-        } finally {
-            if (fos != null) 
-                try { 
-                    fos.close(); 
-                } catch (IOException ex) {
-
-                }
         }
     }
 }
